76f5b189d5528b29155498a1c7ca27fffcbf64ae
Richard <Richard>
2020-04-04 07:05:26 -0700

    fix

--- lesson-02-main/src/lib.rs
+++ lesson-02-main/src/lib.rs
@@ -1,7 +1,8 @@
-extern crate alloc;
-use crate::alloc::string::ToString;
-use alloc::vec::Vec;
-use watson::*;
+#![no_std]
+#![feature(core_intrinsics, lang_items, alloc_error_handler)]
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[no_mangle]
 fn malloc(size: usize) -> *mut u8 {
@@ -10,6 +11,30 @@ fn malloc(size: usize) -> *mut u8 {
     core::mem::forget(buf);
     ptr
 }
+
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &::core::panic::PanicInfo) -> ! {
+    unsafe {
+        ::core::intrinsics::abort();
+    }
+}
+
+// Need to provide an allocation error handler which just aborts
+// the execution with trap.
+#[alloc_error_handler]
+#[no_mangle]
+pub extern "C" fn oom(_: ::core::alloc::Layout) -> ! {
+    unsafe {
+        ::core::intrinsics::abort();
+    }
+}
+extern crate alloc;
+use alloc::string::{String,ToString};
+use alloc::vec::Vec;
+use watson::*;
+
+extern crate wee_alloc;
 
 extern "C" {
     fn _log(msg: *const u8);
@@ -22,7 +47,7 @@ fn log(msg: &str) {
 }
 
 fn load_and_run_main(wasm_bytes: &[u8]) -> Result<f64, String> {
-    let program = Program::parse(&wasm_bytes)?;
+    let program = watson::parse(&wasm_bytes)?;
     let main_function = program.find_exported_function("main")?;
     let main_code = program.find_code_block(main_function.index)?;
     if let Instruction::I32Const(num) = main_code.code_expression[0] {
